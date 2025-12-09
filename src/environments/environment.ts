914cc809f064751c79e427820d490d59fceed3a8
Rushikesh Ganachari <rushikeshganachari111@gmail.com>
2025-12-09 21:07:47 +0530

    integrate firebase manually

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+    production: false,
+    firebase : {
+        apiKey: "AIzaSyAunhj6K2zwm2G852qcT3R7_ur4ijJhVqo",
+        authDomain: "med-tracker-f5391.firebaseapp.com",
+        projectId: "med-tracker-f5391",
+        storageBucket: "med-tracker-f5391.firebasestorage.app",
+        messagingSenderId: "698042821242",
+        appId: "1:698042821242:web:b6b057162f1e969fbd69c3",
+        measurementId: "G-SJTVGGCYCQ"
+    }
+};
